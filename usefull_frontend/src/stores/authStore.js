151d79dfb1d67252bf18dc52d5347b64c15d3646
“Aurel” <aurel.ayoyide@epitech.eu>
2025-10-17 10:42:51 +0100

    [Add]: of the front_end of the evaluation

---
+++ usefull_frontend/src/stores/authStore.js
@@ -0,0 +1,30 @@
+import { defineStore } from "pinia";
+
+const API_URL = "http://127.0.0.1:8000/api"; 
+
+export const useUserStore = defineStore("user", {
+  state: () => ({
+    user: null,
+    token: localStorage.getItem("token") || null,
+    users: []
+  }),
+   persist: true,
+
+  actions: {
+    async register(payload) {
+      const res = await fetch(`${API_URL}/register`, {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify(payload),
+      });
+
+      if (!res.ok) throw await res.json();
+
+      const data = await res.json();
+      this.user = data.user;
+      this.token = data.token;
+      localStorage.setItem("token", this.token);
+    },
+
+  }
+});
---
+++ usefull_frontend/src/views/Navbar.vue
@@ -0,0 +1,25 @@
+<template>
+    <header>
+        <div class="max-w-7xl mx-auto px-6 py-4">
+            <div class="flex items-center justify-between">
+                <div class="flex items-center space-x-8">
+                </div>
+                <div class="flex items-center space-x-4">
+                    <RouterLink to="/login"
+                        class="cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap rounded-md text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive hover:bg-accent dark:hover:bg-accent/50 h-9 px-4 py-2 has-[>svg]:px-3 text-[#64748B] hover:text-[#2563EB]">
+                        Se connecter
+                    </RouterLink>
+                    <RouterLink to="/register"
+                        class="cursor-pointer inline-flex items-center justify-center gap-2 whitespace-nowrap text-sm font-medium transition-all disabled:pointer-events-none disabled:opacity-50 [&_svg]:pointer-events-none [&_svg:not([class*='size-'])]:size-4 shrink-0 [&_svg]:shrink-0 outline-none focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px] aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive h-9 py-2 has-[>svg]:px-3 bg-[#2563EB] hover:bg-blue-700 text-white rounded-lg px-6">
+                        S'inscrire
+                    </RouterLink>
+                    
+                </div>
+            </div>
+        </div>
+    </header>
+</template>
+
+<script setup>
+import { RouterLink } from 'vue-router';
+</script>
