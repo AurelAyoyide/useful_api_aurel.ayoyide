935d24f6f77bb11156f3f638c74379391322f14b
“Aurel” <aurel.ayoyide@epitech.eu>
2025-10-17 12:52:54 +0100

    [Update]: block the acces to user who is not connected

--- usefull_frontend/src/router/index.js
+++ usefull_frontend/src/router/index.js
@@ -2,15 +2,32 @@ import { createRouter, createWebHistory } from "vue-router";
 import Login from "../views/Login.vue";
 import Register from "../views/Register.vue";
 import Dashboard from "../views/Dashboard.vue";
+import { useUserStore } from "../stores/authStore";
+
+
+
 const routes = [
   { path: "/login", component: Login },
   { path: "/register", component: Register },
-  { path: "/dashboard", component: Dashboard },
+  { path: "/dashboard", component: Dashboard, meta: { requiresAuth: true },},
 ];
 
 const router = createRouter({
   history: createWebHistory(),
   routes,
 });
+
+router.beforeEach((to, next) => {
+  const userStore = useUserStore();
+
+  const requiresAuth = to.matched.some((record) => record.meta.requiresAuth);
+
+  if (requiresAuth && !userStore.token) {
+    next("/login");
+  } else {
+    next();
+  }
+});
+
 
 export default router;
--- usefull_frontend/src/views/Dashboard.vue
+++ usefull_frontend/src/views/Dashboard.vue
@@ -79,7 +79,7 @@
                     <span class="mx-4 font-medium">Tickets</span>
                 </a>
 
-                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700"
+                <a class="flex items-center px-4 py-2 mt-5 text-gray-600 transition-colors duration-300 transform rounded-md dark:text-gray-400 hover:bg-gray-100 dark:hover:bg-gray-800 dark:hover:text-gray-200 hover:text-gray-700 cursor-pointer"
                     @click="logout">
                     <svg class="w-5 h-5" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
                         <g id="SVGRepo_bgCarrier" stroke-width="0"></g>
